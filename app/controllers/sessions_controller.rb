ad21799648922f50e3b020c873391d9e57ee59ef
Ibrahim Ahmed <ia.ibrahim@gmail.com>
2019-04-29 21:19:24 +0200

    Changed successful Login respone to match the frontend json decoder.
    Former-commit-id: a6bd0b935ce5edd78610d7bd619b2388cce0ec33

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,14 @@ class SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: resource, except: [:id]
+    render json: { user: { username: resource.email, token: current_token , image: ""}}
   end
 
   def respond_to_on_destroy
     head :no_content
+  end
+
+def current_token
+    request.env['warden-jwt_auth.token']
   end
 end
